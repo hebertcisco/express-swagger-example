a05b5d8a42cc89fcef6669bd96f29169b7e8033e
hebertcisco <hebertcisco@outlook.com>
2022-03-01 15:39:50 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import express from "express";
+import router from "./routes.js";
+const app = express();
+
+app.use(router);
+export default app;
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import * as swaggerUi from "swagger-ui-express";
+const router = Router();
+
+const options = {
+    swaggerOptions: {
+        url: 'http://petstore.swagger.io/v2/swagger.json'
+    }
+}
+router.use("/docs", swaggerUi.serve, swaggerUi.setup(null, options));
+
+export default router;
